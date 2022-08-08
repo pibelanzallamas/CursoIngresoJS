a82e148826aa7ad0a14ed013ac46b0367477ae33
pibelanzallamas <notasbrandon@icloud.com>
2022-08-08 09:11:56 -0300

    texto

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,30 +1,44 @@
 function mostrar()
 {
     let sumaDias;
+    let edad;
     let edadEnDias;
-    let diferenciaAnoActual;
-    let diferenciaAnoPasado;
     let ndia;
     let nmes;
-    let edad;
+    let diferenciaAnoPasado;
     let fdia;
     let fmes;
+    let diferenciaAnoActual;
 
     sumaDias = 0;
-    edadEnDias = 0
-    diferenciaAnoActual = 0;
-    diferenciaAnoPasado = 0;
+    edad     = 0;
+    edadEnDias = 0;
     ndia  = 0;
     nmes  = 0;
-    edad  = 0;
+    diferenciaAnoPasado = 0;
     fdia  = 0;
     fmes  = 0;
+    diferenciaAnoActual = 0;
 
-    ndia = parseInt(prompt("Ingrese su dia de nacimiento"));
-    nmes = parseInt(prompt("Ingrese su mes de nacimiento"));
-    edad = parseInt(prompt("Ingrese su edad"));
-    fmes = parseInt(prompt("Que mes este?"));
-    fdia = parseInt(prompt("Que fecha estamos?"))
+    do{
+        ndia = parseInt(prompt("Ingrese su dia de nacimiento"));
+    }while(isNaN(ndia) || ndia < 1 || ndia > 30)
+    
+    do{
+        nmes = parseInt(prompt("Ingrese su mes de nacimiento"));
+    }while(isNaN(nmes) || nmes < 1 || nmes > 12)
+    
+    do{
+        edad = parseInt(prompt("Ingrese su edad"));
+    }while(isNaN(edad) || edad < 0 || edad > 120)
+
+    do{
+        fmes = parseInt(prompt("Que mes este?"));
+    }while(isNaN(fmes) || fmes < 0 || fmes > 12)
+
+    do{
+        fdia = parseInt(prompt("Que fecha estamos?"));
+    }while(isNaN(fdia) || fdia < 0 || fdia > 30)
 
     edadEnDias = edad * 365;
     diferenciaAnoPasado = (30 - ndia) + ((12 - nmes) * 30);
@@ -33,5 +47,4 @@ function mostrar()
     sumaDias = edadEnDias + diferenciaAnoActual + diferenciaAnoPasado;
 
     alert("Felicidades usted lleva " + sumaDias + " días en la tierra! ");
-
 }
