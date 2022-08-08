c7ce6c03a6c1a39fd0d226dd8df8332e1a5949d9
pibelanzallamas <notasbrandon@icloud.com>
2022-08-08 13:05:05 -0300

    final version

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -33,6 +33,11 @@ function mostrar()
     }while(isNaN(edad) || edad < 0 || edad > 120)
 
     do{
+        bandera = prompt("Los ha cumplido este ano? si/no");
+        bandera = bandera.toLowerCase();
+    }while(!(bandera == "si" || bandera == "no"))
+
+    do{
         fmes = parseInt(prompt("Que mes este?"));
     }while(isNaN(fmes) || fmes < 0 || fmes > 12)
 
@@ -40,10 +45,15 @@ function mostrar()
         fdia = parseInt(prompt("Que fecha estamos?"));
     }while(isNaN(fdia) || fdia < 0 || fdia > 30)
 
-    edadEnDias = edad * 365;
-    diferenciaAnoPasado = (30 - ndia) + ((12 - nmes) * 30);
-    diferenciaAnoActual = fdia + ((fmes - 1) * 30);
+    if(bandera == "si"){
+        diferenciaAnoActual = (30 - ndia) + ((fmes - nmes - 1) * 30) + fdia;
+    }else{
+        diferenciaAnoPasado = (30 - ndia) + ((12 - nmes) * 30);
+        diferenciaAnoActual = fdia + ((fmes - 1) * 30); 
+    }
 
+    edadEnDias = edad * 365.256;
+    edadEnDias = parseInt(edadEnDias);
     sumaDias = edadEnDias + diferenciaAnoActual + diferenciaAnoPasado;
 
     alert("Felicidades usted lleva " + sumaDias + " días en la tierra! ");
